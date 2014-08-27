017477dbb9d08926ce55d03d33aaa35f33c390af
jenniebarrella <jeb518@lehigh.edu>
2014-08-27 22:48:45 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+ // define a class
+ public class WelcomeClass {
+   
+   // add a main method
+   public static void main(String[ ] args) {
+     
+     //print the statement with the box around it
+     System.out.println("     -------------");
+     System.out.println("     |  WELCOME  |");
+     System.out.println("     -------------");
+     System.out.println("    ^  ^  ^  ^  ^  ^");
+     System.out.println("   / \\/ \\/ \\/ \\/ \\/ \\");
+     System.out.println("  |-J--E--B--5--1--8-|");
+     System.out.println("   \\ /\\ /\\ /\\ /\\ /\\ /");
+     System.out.println("    v  v  v  v  v  v ");
+     System.out.println("This summer I went to the beach and worked at an ice cream shop");
+     
+     //test
+     
+   }
+ }
