f02ab60012c62bb51b057effeba85e54aa400bf8
jenniebarrella <jeb518@lehigh.edu>
2014-10-13 20:47:43 +0000

    hw06 update root

--- hw06/Root.java
+++ hw06/Root.java
@@ -2,49 +2,51 @@
 // October 10, 2014 
 // CSE 2
 // hw06
+// find the approximate square root of the entered double
 
-import java.util.Scanner;
+import java.util.Scanner; // inport scanner
 
-public class Root {
-    public static void main(String [ ] args) {
+public class Root { // declare class
+    public static void main(String [ ] args) { // declare main method
         
-        Scanner myScanner = new Scanner (System.in);
+        Scanner myScanner = new Scanner (System.in); // declare scanner input
         
-        System.out.print("Enter a double greater than 0: ");
+        System.out.print("Enter a double greater than 0: ");  // input statement
         
-        if (myScanner.hasNextDouble()) {
+        if (myScanner.hasNextDouble()) { // make sure user enters double
             
-            double x = myScanner.nextDouble();
+            double x = myScanner.nextDouble(); // set variable equal to input
             
-            if (x>0) {
+            if (x>0) { // make sure input is positive
                 
+                // declare variables
                 double low = 0;
                 double high = 1 + x;
                 double middle = 0;
                 
-                while ( ( high-low ) > 0.0000001*( 1+x ) ) {
-                    middle = (high+low)/2;
-                    if ( (middle*middle) > x) {
+                while ( ( high-low ) > 0.0000001*( 1+x ) ) { // while statement to stop when high low difference is very small
+                    middle = (high+low)/2; // find the number between high and low
+                    if ( (middle*middle) > x) { // if middle number is larger than input, middle is the new high
                         high = middle;
                     }  
-                    else {
+                    else { // if the middle number is smaller than input, middle is the new low
                         low = middle;
                     }
             
-                }
+                } // end while
                 
-                System.out.println("The square root is "+middle);
+                System.out.println("The square root is "+middle); // final print statement
                 
             }
             
             else { 
-                System.out.println("you did not enter an appropriate value");
+                System.out.println("you did not enter an appropriate value"); // incorrect input
             }    
             
         }
         
         else {
-            System.out.println("you did not enter an appropriate value");
+            System.out.println("you did not enter an appropriate value"); // incorrect input
         }
         
     } // end main method
